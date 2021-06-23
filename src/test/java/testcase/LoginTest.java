769d816eb0cd4c67c016a6c492d3b6f6457fad95
“sunxiaohua92” <352393384@qq.com>
2021-06-23 14:30:55 +0800

    first commit

---
+++ src/test/java/testcase/LoginTest.java
@@ -0,0 +1,19 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkins_test
+ * @Site: 天道酬勤！！
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 版权所有 湖南省零檬信息技术有限公司
+ * @Author: Admin
+ * @Create: 2021-06-23 11:22
+ * @Desc：
+ **/
+public class LoginTest {
+    @Test
+    public  void  loginSuccess(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/testcase/RegissterTest.java
@@ -0,0 +1,19 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkins_test
+ * @Site: 天道酬勤！！
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 版权所有 湖南省零檬信息技术有限公司
+ * @Author: Admin
+ * @Create: 2021-06-23 11:24
+ * @Desc：
+ **/
+public class RegissterTest {
+    @Test
+    public  void registerSuccess(){
+        System.out.println("注册成功");
+    }
+}
